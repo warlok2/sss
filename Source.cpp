5cd10c446a5a7f7663899b4fdc7f89fceec59025
warlok2 <114303994+warlok2@users.noreply.github.com>
2022-09-24 19:19:28 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*int a;
+	bool adolf;
+	cin >> a;
+	adolf = a > 0;
+	cout << boolalpha << adolf << endl;*/
+	/*int a;
+	bool d;
+	cin >> a;
+	d = a % 2 != 0;
+	cout << boolalpha << d << endl;*/
+	/*int a;
+	bool d;
+	cin >> a;
+	d = a % 2 == 0;
+	cout << boolalpha << d << endl;*/
+	/*int a, b;
+	cin >> a >> b;
+	bool d;
+	d = a > 2;
+	bool f;
+	f = b <= 3;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl;*/
+	/*int a, b;
+	bool d;
+	cin >> a >> b;
+	d = a >= 0;
+	bool f;
+	f = b < -2;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl;*/
+	/*int a, b, c;
+	bool d;
+	cin >> a >> b >> c;
+	d = a ||<  b ||<   c||;
+	cout << boolalpha << d << endl;*/
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	bool d;
+	d = a > b < c;
+	cout << boolalpha << d << endl;*/
+
+	/*int a, b;
+	cin >> a >> b;
+	bool d;
+	d = a % 2 == 0;
+	bool f;
+	f = b % 2 == 0;
+	cout << boolalpha << d << endl;
+	cout << boolalpha <<f << endl;*/
+	/*int a, b;
+	cin >> a >> b;
+	bool d;
+	d = a % 2 != 0;
+	bool f;
+	f = b % 2 != 0;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl;*/
+	/*int a, b;
+	cin >> a >> b;
+	bool d;
+	d = a % 2 != 0;
+	bool f;
+	f = b % 2 == 0;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl;*/
+	/*int a, b;
+	cin >> a >> b;
+	bool d;
+	d = a % 2 == 0 && b % 2 == 0;
+	bool f;
+	f = a % 2 != 0 && b % 2 != 0;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl;*/
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	bool d;
+	d = a > 0 && b > 0 && c > 0;
+	cout << boolalpha << d << endl;*/
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	bool d;
+	d = a > 0 && b > 0 && c > 0;
+	cout << boolalpha << d << endl;*/
+	int a, b, c;
+	cin >> a >> b >> c;
+	bool d;
+	bool f;
+	d = a > 0 && b > 0 && c > 0;
+	d = a < 0 && b < 0 && c < 0;
+	cout << boolalpha << d << endl;
+	cout << boolalpha << f << endl; 
+	
+
+	
+
+
+	
+
+
+
+
+}
